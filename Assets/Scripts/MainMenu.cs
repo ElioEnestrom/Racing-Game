a373de51f2a6cb78dd5a537a44c5fa42745e4076
ElioEnestrom <elio@enestrom.com>
2023-10-09 15:22:38 +0200

    Made a pause

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -2,15 +2,43 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.InputSystem;
 
 public class MainMenu : MonoBehaviour
 {
+    public InputAction pauseMenu;
+    public GameObject mainMenuObject;
+
+    public void Awake()
+    {
+
+    }
+    private void OnEnable()
+    {
+        pauseMenu.performed += ctx => Pause();
+        pauseMenu.Enable();
+    }
     public void PlayGame()
     {
+        Time.timeScale = 1;
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-    }public void ExitGame()
+    }
+    public void Menu()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+    }
+    public void ExitGame()
     {
         Application.Quit();
+    }
+    public void Resume()
+    {
+        Time.timeScale = 1;
+    }
+    public void Pause()
+    {
+        mainMenuObject.SetActive(true);
+        Time.timeScale = 0;
     }
     public void PlaySettings()
     {
